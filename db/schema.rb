45fb3572d2164cbd0d81f2f8264d47afff61196a
Gabriel Oliveira <gogvale2011@gmail.com>
2021-09-25 15:50:42 -0500

    Change from sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_09_25_155433) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "body", null: false
     t.datetime "created_at", precision: 6, null: false
