118dca74592dd0f13da8144fde850a6061343e19
Gabriel Oliveira <gogvale2011@gmail.com>
2021-09-25 10:59:37 -0500

    Finish post and user model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_25_023753) do
+ActiveRecord::Schema.define(version: 2021_09_25_155433) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "Anonymous", null: false
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_09_25_023753) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
